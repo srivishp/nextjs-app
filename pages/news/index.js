ca13c833c48563636b43b5cf9b877a2fa3c83310
Sri Vishnu <srivishp@gmail.com>
2023-10-27 12:54:06 +0530

    Add nested paths

---
+++ pages/news/index.js
@@ -0,0 +1,10 @@
+// our-domain.com/news
+
+// * NEXT JS has file based routing.
+// ie: Folder & file structure (file paths) in the project defines the nesting of web pages
+
+function NewsPage() {
+  return <h1> The News Page</h1>;
+}
+
+export default NewsPage;
---
+++ pages/news/something-important.js
@@ -0,0 +1,7 @@
+// our-domain.com/news/something-important
+
+function ImportantPage() {
+  return <h1> The Important Page</h1>;
+}
+
+export default ImportantPage;
