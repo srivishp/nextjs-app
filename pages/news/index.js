5fe76f602cbfc33727d3a36fe2d77cdab994ac7c
Sri Vishnu <srivishp@gmail.com>
2023-10-27 14:26:22 +0530

    Dynamic pages & linking

--- pages/news/index.js
+++ pages/news/index.js
@@ -3,8 +3,21 @@
 // * NEXT JS has file based routing.
 // ie: Folder & file structure (file paths) in the project defines the nesting of web pages
 
+// Link makes it a single page application
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1> The News Page</h1>;
+  return (
+    <>
+      <h1> The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/newsarticleone">News Article 1</Link>
+        </li>
+        <li>News Article 2</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
